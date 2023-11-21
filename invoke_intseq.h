78d7d95c21c31a41e7f05d9bc8d66272b523a545
bartek <bartek998a@gmail.com>
2023-11-21 18:11:54 +0100

    Added an if checking whether any of the args is an integer sequence.

--- invoke_intseq.h
+++ invoke_intseq.h
@@ -1,9 +1,28 @@
 #ifndef INVOKE_INTSEQ
 #define INVOKE_INTSEQ
 
+#include <functional>
 #include <iostream>
+#include <type_traits>
 
-typedef int result_type;
+namespace invoke_inteq_details {
+
+template <typename T> struct is_integer_sequence : std::false_type {};
+
+template <typename T, T... vals>
+struct is_integer_sequence<std::integer_sequence<T, vals...>> : std::true_type {
+};
+
+// checks if any of the others are an integer_sequence
+template <class... others>
+struct any_match : std::disjunction<is_integer_sequence<others>...> {};
+} // namespace invoke_inteq_details
+
 template <class F, class... Args>
-result_type invoke_intseq(F&& f, Args&&... args);
+constexpr auto invoke_intseq(F &&f, Args &&...args) {
+    if constexpr (std::is_same<std::false_type, invoke_inteq_details::any_match<Args...>>::value) {
+        return std::invoke(f, args...);
+    } else {
+    }
+}
 #endif // !INVOKE_INTSEQ
