052f39266537ea8d02a436399b516e91203b7fb4
bartek <bartek998a@gmail.com>
2023-11-25 13:01:39 +0100

    Added initial loop to fin the integer_sequence

--- invoke_intseq.h
+++ invoke_intseq.h
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <type_traits>
 #include <optional>
+#include <tuple>
 
 namespace invoke_inteq_details {
 
@@ -21,32 +22,43 @@ struct any_match : std::disjunction<is_integer_sequence<others>...> {};
 
 // Finds the first element that is an integer sequence
 // Assumes that arguments contain integer sequence
-template <typename First, typename ...Rest>
+template <class F, std::tuple t, typename First, typename ...Rest>
 struct find_first_struct {
-    constexpr auto findFirst(First&& first, Rest&& ...rest){
-        if constexpr (is_integer_sequence<First>::value){
-            return first;
-        } else {
-            return findFirst(rest ...);
-        }
+    constexpr auto findFirst(F&& f, First&& first, Rest&& ...rest){
+        return findFirst<t, Rest...>(rest ...);
     }
 };
 
-template<typename First>
-struct find_first_struct<First>{
-    constexpr auto findFirst(First&& first){
+// found the element
+template <class F,std::tuple t, typename T, T... vals, typename ...Rest>
+struct find_first_struct<F ,t, std::integer_sequence<T, vals...>, Rest...> {
+    constexpr auto findFirst(F&& f, T&& first, Rest&& ...rest){
+        return std::array<std::result_of<decltype(f)()>, sizeof... (vals)> (expandRecursively);
+    }
+};
+
+template<class  F,std::tuple t, typename First>
+struct find_first_struct<F, t, First>{
+    constexpr auto findFirst(F&& f,First&& first){
         if constexpr (is_integer_sequence<First>::value){
             return first;
         }
     }
+
+    template<>
+    constexpr auto expandRecursively(){
+
+    }
+
 };
 
 } // namespace invoke_inteq_details
 template <class F, class... Args>
 constexpr auto invoke_intseq(F &&f, Args &&...args) {
-    if constexpr (std::is_same<std::false_type, invoke_inteq_details::any_match<Args...>>::value) {
+    if constexpr (std::is_same<std::false_type, invoke_inteq_details::any_match<Args...>>::value || (sizeof... (args)) == 0) {
         return std::invoke(f, args...);
     } else {
+        return invoke_inteq_details::find_first_struct<std::tuple<Args...>(args...), Args...>::findFirst(args...);
     }
 }
 #endif // !INVOKE_INTSEQ
