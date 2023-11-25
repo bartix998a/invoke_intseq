88e10d31f5371d2c034b9ed290be43c9b6dbd6b8
bartek <bartek998a@gmail.com>
2023-11-25 14:47:55 +0100

    Added a position counter to find_first

--- invoke_intseq.h
+++ invoke_intseq.h
@@ -22,7 +22,7 @@ struct any_match : std::disjunction<is_integer_sequence<others>...> {};
 
 // Finds the first element that is an integer sequence
 // Assumes that arguments contain integer sequence
-template <class F, std::tuple t, typename First, typename ...Rest>
+template <size_t pos, class F, std::tuple t, typename First, typename ...Rest>
 struct find_first_struct {
     constexpr auto findFirst(F&& f, First&& first, Rest&& ...rest){
         return findFirst<t, Rest...>(rest ...);
@@ -30,15 +30,15 @@ struct find_first_struct {
 };
 
 // found the element
-template <class F,std::tuple t, typename T, T... vals, typename ...Rest>
-struct find_first_struct<F ,t, std::integer_sequence<T, vals...>, Rest...> {
+template <size_t pos, class F,std::tuple t, typename T, T... vals, typename ...Rest>
+struct find_first_struct<pos, F ,t, std::integer_sequence<T, vals...>, Rest...> {
     constexpr auto findFirst(F&& f, T&& first, Rest&& ...rest){
         return std::array<std::result_of<decltype(f)()>, sizeof... (vals)> (expandRecursively);
     }
 };
 
-template<class  F,std::tuple t, typename First>
-struct find_first_struct<F, t, First>{
+template<size_t pos, class  F,std::tuple t, typename First>
+struct find_first_struct<pos, F, t, First>{
     constexpr auto findFirst(F&& f,First&& first){
         if constexpr (is_integer_sequence<First>::value){
             return first;
@@ -58,7 +58,7 @@ constexpr auto invoke_intseq(F &&f, Args &&...args) {
     if constexpr (std::is_same<std::false_type, invoke_inteq_details::any_match<Args...>>::value || (sizeof... (args)) == 0) {
         return std::invoke(f, args...);
     } else {
-        return invoke_inteq_details::find_first_struct<std::tuple<Args...>(args...), Args...>::findFirst(args...);
+        return invoke_inteq_details::find_first_struct<0, F, std::tuple<Args...>(args...), Args...>::findFirst(f, args...);
     }
 }
 #endif // !INVOKE_INTSEQ
